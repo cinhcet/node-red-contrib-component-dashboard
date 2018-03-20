efbbff17103e19adfea91aea3dc38a302cf64309
Danny <danny.driess@gmx.de>
2018-03-20 15:49:32 +0100

    white space

--- yad.js
+++ yad.js
@@ -4,9 +4,9 @@ var initialized = false;
 
 module.exports = function(RED) {
   var serveStatic = require('serve-static');
-  var	socketIO = require('socket.io');
-  var	path = require('path');
-  var	fs = require('fs');
+  var socketIO = require('socket.io');
+  var path = require('path');
+  var fs = require('fs');
 
   function yad(config) {
     RED.nodes.createNode(this, config);
@@ -15,18 +15,18 @@ module.exports = function(RED) {
     node.elementNodes = {};
 
     var server = RED.server;
-	  var app = RED.httpNode || RED.httpAdmin;
+    var app = RED.httpNode || RED.httpAdmin;
 
     var yadPath = 'yad';
 
-	  var fullPath = join(RED.settings.httpNodeRoot, yadPath);
-	  var socketIoPath = join(fullPath, 'socket.io');
+    var fullPath = join(RED.settings.httpNodeRoot, yadPath);
+    var socketIoPath = join(fullPath, 'socket.io');
     node.io = socketIO(server, {path: socketIoPath});
 
     app.use(join(yadPath), serveStatic(path.join(__dirname, "src")));
-	  node.log("YAD started at " + fullPath);
+    node.log("YAD started at " + fullPath);
 
-	  node.io.on('connection', function(socket) {
+    node.io.on('connection', function(socket) {
       socket.on('toNR', function(msg) {
         if(msg.hasOwnProperty('elementID') && msg.hasOwnProperty('msg')) {
           if(node.elementNodes.hasOwnProperty(msg.elementID)) {
@@ -34,11 +34,11 @@ module.exports = function(RED) {
           }
         }
       });
-	  });
+    });
 
     node.io.on('connect_error', function (error) {
       node.warn('connect_error', error);
-	  });
+    });
   }
 
   yad.prototype.sendMessage = function(elementNode, msg) {
@@ -69,7 +69,7 @@ module.exports = function(RED) {
 
 //from http://stackoverflow.com/a/28592528/3016654
 function join() {
-	var trimRegex = new RegExp('^\\/|\\/$','g'),
-	paths = Array.prototype.slice.call(arguments);
-	return '/' + paths.map(function(e){return e.replace(trimRegex,"");}).filter(function(e){return e;}).join('/');
+  var trimRegex = new RegExp('^\\/|\\/$','g'),
+  paths = Array.prototype.slice.call(arguments);
+  return '/' + paths.map(function(e){return e.replace(trimRegex,"");}).filter(function(e){return e;}).join('/');
 }
